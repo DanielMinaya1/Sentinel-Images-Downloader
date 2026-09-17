fc8282a8bc3eccc01b42bd5b211e313f7b5668d1
DanielMinaya1 <Daniely41149512.>
2026-09-17 18:37:41 -0300

    add funcName to logs

--- downloader/config/logger.py
+++ downloader/config/logger.py
@@ -13,7 +13,7 @@ def setup_logger(file_name: str | pathlib.Path) -> logging.Logger:
         logger.handlers.clear()
 
     formatter = logging.Formatter(
-        fmt="%(asctime)s - %(levelname)s - %(message)s",
+        fmt="%(asctime)s - %(funcName)s - %(levelname)s - %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
     )
 
